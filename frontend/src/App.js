ee4945e98b4906873f7582225df60b4b5e5bb72d
MattMatt-aka-Matt <78562870+MattMatt-aka-Matt@users.noreply.github.com>
2025-12-02 14:38:12 +0100

    app js upgrade

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Components
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import CookieBanner from './components/CookieBanner';
+import ProtectedRoute from './components/ProtectedRoute';
+import AdminRoute from './components/AdminRoute';
+
+// Pages
 import ProductList from './pages/ProductList';
 import Cart from './pages/Cart';
 import Login from './pages/Login';
@@ -9,40 +17,69 @@ import Register from './pages/Register';
 import Order from './pages/Order';
 import Admin from './pages/Admin';
 import ShippingPayment from './pages/ShippingPayment';
-import { CartProvider } from './context/CartContext';
-import ProtectedRoute from './components/ProtectedRoute';
-import AdminRoute from './components/AdminRoute';
-// Nouvelles pages RGPD
+
+// Pages RGPD
 import Privacy from './pages/Privacy';
 import Legal from './pages/Legal';
 import Terms from './pages/Terms';
-import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
-import CookieBanner from './components/CookieBanner.js';
+import NotFound from './pages/NotFound';
+
+// Context
+import { CartProvider } from './context/CartContext';
 
 function App() {
   return (
     <CartProvider>
       <Router>
-        <Navbar />
-        <CookieBanner />
-        <Routes>
-          <Route path="/" element={<ProductList />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/order" element={<ProtectedRoute><Order /></ProtectedRoute>} />
-          <Route path="/shipping-payment" element={<ProtectedRoute><ShippingPayment /></ProtectedRoute>} />
-          <Route path="/admin" element={<AdminRoute><Admin /></AdminRoute>} />
-          <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-          {/* Pages RGPD */}
-          <Route path="/privacy" element={<Privacy />} />
-          <Route path="/legal" element={<Legal />} />
-          <Route path="/terms" element={<Terms />} />
-          {/* 404 */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
+        <div className="min-h-screen flex flex-col">
+          <Navbar />
+          <CookieBanner />
+          
+          <main className="flex-grow">
+            <Routes>
+              {/* Pages publiques */}
+              <Route path="/" element={<ProductList />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+
+              {/* Pages protégées (utilisateur connecté) */}
+              <Route path="/order" element={
+                <ProtectedRoute>
+                  <Order />
+                </ProtectedRoute>
+              } />
+              <Route path="/shipping-payment" element={
+                <ProtectedRoute>
+                  <ShippingPayment />
+                </ProtectedRoute>
+              } />
+              <Route path="/profile" element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              } />
+
+              {/* Pages admin */}
+              <Route path="/admin" element={
+                <AdminRoute>
+                  <Admin />
+                </AdminRoute>
+              } />
+
+              {/* Pages RGPD */}
+              <Route path="/privacy" element={<Privacy />} />
+              <Route path="/legal" element={<Legal />} />
+              <Route path="/terms" element={<Terms />} />
+
+              {/* 404 - doit être en dernier */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+
+          <Footer />
+        </div>
       </Router>
     </CartProvider>
   );
